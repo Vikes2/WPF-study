9d0000fd174cc65244ab5281242dc3227eea1592
Vikes2 <muczynski.pawel@gmail.com>
2018-04-18 22:12:43 +0200

    add the 7th task

---
+++ zad7wpf/zad7wpf/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zad7wpf
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Person> Persons = new ObservableCollection<Person>(); 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ListPersons.ItemsSource = Persons;
+            RegionCB.ItemsSource = Enum.GetValues(typeof(CitiesEnum));
+            
+            
+        }
+
+        
+
+        private void ListBoxItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Persons.Add(new Person());
+            
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Persons.Remove((Person)ListPersons.SelectedItem);
+        }
+    }
+}
---
+++ zad7wpf/zad7wpf/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad7wpf
+{
+    public enum CitiesEnum
+    {
+        Bialystok,
+        Warszawa,
+        Gdansk ,
+        Poznan,
+        Katowice,
+        Sopot
+    }
+
+    public class Person : INotifyPropertyChanged
+    {
+        private string name;
+        private string lastName;
+        private string mail;
+        public decimal Price { get; set; }
+        public CitiesEnum City { get; set; }
+        public int AccessLevel { get; set; }
+        private bool description;
+
+
+        public bool Description
+        { get { return description;}
+          set { description = value; OnPropertyChanged("Description");}
+        }
+
+        public Person()
+        {
+            Price = 0;
+            AccessLevel = 0;
+            Description = true;
+            City = 0 ;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged("personView"); }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; OnPropertyChanged("personView"); }
+        }
+
+        public string Mail
+        {
+            get { return mail; }
+            set { mail = value; OnPropertyChanged("personView"); }
+        }
+
+        public string PersonView
+        {
+            
+            get {
+                if(mail == null)
+                {
+                    return name + " " + lastName;
+                }
+                return name + " " + lastName + " (" + mail + ")"; }
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this,
+                new PropertyChangedEventArgs(property));
+        }
+
+    }
+}
